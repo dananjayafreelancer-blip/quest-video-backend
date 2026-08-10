5ea15afa75a66a54f7a38fb4ab1c930ea7f68201
dananjayafreelancer-blip <dananjayafreelancer@gmail.com>
2026-08-10 09:03:15 +0530

    index.jsx

---
+++ src/index.jsx
@@ -0,0 +1,7 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { RemotionRoot } from "./Root";
+
+const root = createRoot(document.getElementById("root"));
+
+root.render(<RemotionRoot />);
